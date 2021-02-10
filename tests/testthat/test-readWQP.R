8a948e607186a9742cdd105ce7e1bd3746b6d205
mthawley <thawley.michelle@epa.gov>
2021-02-10 17:16:52 -0500

    Just adding comments.

--- tests/testthat/test-readWQP.R
+++ tests/testthat/test-readWQP.R
@@ -2,10 +2,10 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("read query from file works", {
+test_that("test simple data query using agruments", {
   df <- readWQP(statecode="WV",countycode='037',characteristicName="Atrazine")
   # this is a lousy test...
   numrec <- nrow(df)
-  expect_equal(numrec, 35)  # replace this with a df comparision?
+  expect_equal(numrec, 35)  # replace this with a df comparison?
 
 })
